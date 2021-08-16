c1c750e52e6894f37976c4147489da8ae01f610d
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-16 22:37:15 +0700

    Add project files.

---
+++ SK-Lib-cmath-isinf-Bagian3__CPP/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	cout << "isinf(0.0) is = " << isinf(0.0) << endl;
+	cout << "isinf(sqrt(-1.0)) is = " << isinf(sqrt(-1.0)) << endl;
+
+	_getch();
+	return 0;
+}
